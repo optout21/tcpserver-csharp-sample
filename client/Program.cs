162927c62b2fbf0fbe901bf151b750e83102cafa
adam <adam_rotaru@yahoo.com>
2019-10-22 12:25:45 +0200

    Created lib+exes project structure, with a SLN.

---
+++ client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tcpserver_csharp_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TCP CSharp Client");
+
+            ClientApp app = new ClientApp();
+            app.Start(new AppParams(5000, 5));
+
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace tcpserver_csharp_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TCP CSharp Server");
+
+            ServerApp app = new ServerApp();
+            app.Start(new AppParams(5000, 5));
+
+            Console.Write("Press Enter to exit ...");
+            Console.ReadLine();
+            Console.WriteLine();
+
+            app.Stop();
+            Console.WriteLine("Done");
+        }
+    }
+}
